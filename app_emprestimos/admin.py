d9633ca81434155a82160d8e3b1ac8cb60bc9995
João Carvalho <joao.carvalho@maestrus.com>
2021-09-18 10:29:44 -0300

    Tarefa 1 concluída

--- app_emprestimos/admin.py
+++ app_emprestimos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from app_emprestimos.models import ObjetoEmprestimo
 
-# Register your models here.
+
+class ObjetoAdmin(admin.ModelAdmin):
+
+    search_fields = [
+        'nome',
+    ]
+
+    list_display = ('nome', 'data_aquisicao', 'tipo_objeto')
+
+    def save_model(self, request, obj, form, change):
+        super(ObjetoAdmin, self).save_model(request, obj, form, change)
+
+    def delete_model(self, request, obj):
+        super(ObjetoAdmin, self).delete_model(request, obj)
+
+
+admin.site.register(ObjetoEmprestimo, ObjetoAdmin)
--- app_emprestimos/models.py
+++ app_emprestimos/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class ObjetoEmprestimo(models.Model):
+    TIPO_LIVRO = 1
+    TIPO_CD = 2
+    TIPO_ROUPA = 3
+    TIPO_JOGO = 4
+    TIPO_ELETRONICO = 5
+    TIPO_GERAL = 6
+    TIPO_FERRAMENTA = 7
+
+    TIPO_OBJETO_CHOICES = (
+        (TIPO_LIVRO, 'Livro'),
+        (TIPO_CD, 'CD'),
+        (TIPO_ROUPA, 'Roupa'),
+        (TIPO_JOGO, 'Jogo'),
+        (TIPO_ELETRONICO, 'Eletrônicos'),
+        (TIPO_GERAL, 'Objetos em Geral'),
+        (TIPO_FERRAMENTA, 'Ferramentas em Geral'),
+    )
+
+    nome = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        verbose_name='Nome do Objeto',
+    )
+
+    data_aquisicao = models.DateField(
+        null=False, blank=False,
+        verbose_name='Data de Aquisição',
+    )
+
+    tipo_objeto = models.PositiveSmallIntegerField(
+        null=False, blank=False,
+        verbose_name='Tipo do Objeto',
+        choices=TIPO_OBJETO_CHOICES,
+    )
+
+    class Meta:
+        db_table = 'OBJ_001OBJ'
+        verbose_name = 'Objeto para Empréstimo'
+        verbose_name_plural = 'Objetos para Empréstimo'
+
